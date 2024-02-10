90d085d9e60249ef1250dfbd071379b2bd38cbff
roflmaostc <fxw+git@mailbox.org>
2024-02-10 02:23:40 +0100

    add test case for wave optics

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,9 +26,41 @@ using ChainRulesTestUtils
     	n_resin=1.48
     )
     @test target2 == (0.7 .< optimize_patterns((target2), geometry_vial, optimizer2, LossThreshold(thresholds=(0.65, 0.75)))[2])
+    @test target2 !== (0.7 .> optimize_patterns((target2), geometry_vial, optimizer2, LossThreshold(thresholds=(0.65, 0.75)))[2])
     @test target2 == (0.45 .< optimize_patterns((target2), geometry_vial, optimizer2, LossThreshold(thresholds=(0.4, 0.5)))[2])
+    @test target2 !== (0.45 .> optimize_patterns((target2), geometry_vial, optimizer2, LossThreshold(thresholds=(0.4, 0.5)))[2])
 
 end
+
+
+@testset "Simple wave optical simulation" begin
+
+    sz2 = (24, 24, 24)
+    target = box(Float32, sz2, (17, 17, 10)) .-  box(Float32, sz2, (9, 9, 8));
+
+    n_resin = 1.5f0
+    angles = range(0, π, 20)
+    optimizer = GradientBased(optimizer=Optim.LBFGS(), options=Optim.Options(iterations=15, store_trace=true))
+
+
+    L = 100f-6
+    loss = LossThreshold(thresholds=(0.65, 0.75))
+
+    optimizer = GradientBased(optimizer=Optim.LBFGS(), 		options=Optim.Options(iterations=20, store_trace=true))
+
+    waveoptics = WaveOptics(
+	z=(range(-L/2, L/2, size(target,1))), 
+	L=L, 
+	λ=405f-9 / n_resin, 
+	μ=nothing, 
+	angles=angles,
+	)
+    patterns, printed, res = optimize_patterns(target, waveoptics, optimizer, loss)
+    @test target == (0.7 .< printed)
+    @test target !== (0.7 .> printed) 
+
+end
+
 
 
 @testset "test rrule of custom loss" begin
